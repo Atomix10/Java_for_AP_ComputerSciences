1697414e3b57b00b7cda1c199a5520d462ce869c
Atomix10 <paul14sprenk@gmail.com>
2018-01-11 11:35:45 -0500

    Add files via upload

---
+++ StarName/src/Star_Name.java
@@ -0,0 +1,13 @@
+
+public class Star_Name {
+	public static void main(String[] args)
+	{
+		System.out.println("*****     **   *    *   *");
+		System.out.println("*    *    *  *  *   *   *");
+		System.out.println("*    *   *    * *   *   *");
+		//System.out.
+		
+	}
+
+
+}
